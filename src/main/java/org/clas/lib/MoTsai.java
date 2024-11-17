728bb3e5d921a82d0ddc16634edaa0508e3f6919
L Smith <colesmith@ls-imac.lan>
2024-11-16 21:57:25 -0500

    Update

--- src/main/java/org/clas/lib/MoTsai.java
+++ src/main/java/org/clas/lib/MoTsai.java
@@ -5,16 +5,16 @@ import java.util.Formatter;
 
 public class MoTsai {
 	
-	double gep;             
-	double gmp;            
-	double  pi = 3.141592654;
-	double rmp = 0.93827;
-	double rme = 0.00051099895069;
-	double fsc = 1./137.01;
+    double gep;             
+    double gmp;            
+    double  pi = 3.141592654;
+    double rmp = 0.93827;
+    double rme = 0.00051099895069;
+    double fsc = 1./137.01;
 	
-	public double z0sum=0, z1sum=0, z2sum=0, del_mo, delta_t, xsect_raw, xsect_rad, radcor;
-	public double q2, e_prime;
-	public int param;
+    public double z0sum=0, z1sum=0, z2sum=0, del_mo, delta_t, xsect_raw, xsect_rad, radcor;
+    public double q2, e_prime;
+    public int param;
  
     public MoTsai() {
 		
@@ -24,96 +24,97 @@ public class MoTsai {
 
         double fsc2,theta2,s2,s22,c2,c22,s24,t2,t22,recoil,rmott,tau,eps,sig;
 
-    	fsc2 = fsc*fsc; 
-
-    	theta2 = Math.toRadians(theta)/2.;
-    	s2 = Math.sin(theta2); s22=s2*s2; s24=s2*s2*s2*s2;
-    	c2 = Math.cos(theta2); c22=c2*c2;
-    	t2 = Math.tan(theta2); t22=t2*t2;
-    
-    	recoil = 1.+(2.*eb/rmp)*s22;
-    	e_prime = eb/recoil;
+        fsc2 = fsc*fsc; 
+        
+        theta2 = Math.toRadians(theta)/2.;
+        
+        s2 = Math.sin(theta2); s22=s2*s2; s24=s2*s2*s2*s2;
+        c2 = Math.cos(theta2); c22=c2*c2;
+        t2 = Math.tan(theta2); t22=t2*t2;
+        
+        recoil = 1.+(2.*eb/rmp)*s22;
+        e_prime = eb/recoil;
     	
-    	q2 = 4.*eb*e_prime*s22;    
+        q2 = 4.*eb*e_prime*s22;    
         getFF(q2,param);
 
-    	rmott = 389*fsc2*c22/(4.*eb*eb*s24);   
-    	rmott = rmott/recoil;
+        rmott = 389*fsc2*c22/(4.*eb*eb*s24);   
+        rmott = rmott/recoil;
     
-    	tau = q2/(4.*rmp*rmp);
-    	eps = 1/(1+2*(1+tau)*t22);
+        tau = q2/(4.*rmp*rmp);
+        eps = 1/(1+2*(1+tau)*t22);
     
-    	sig = (tau*gmp*gmp+eps*gep*gep)/eps/(1+tau);
+        sig = (tau*gmp*gmp+eps*gep*gep)/eps/(1+tau);
      
-    	return rmott*sig;
+        return rmott*sig;
     
     }    
     
     public void setFF(int param) {
-    	this.param = param;
+        this.param = param;
     }
     
     public double getBCC(double eb, double theta, double dtheta, int nsamples) { //bin centering correction
-    	double xsect = elas(eb,theta);
-    	double xsum = 0;
-    	for (int i=0; i<nsamples; i++) xsum = xsum + elas(eb,theta + dtheta*(Math.random()-0.5));
+        double xsect = elas(eb,theta);
+        double xsum = 0;
+        for (int i=0; i<nsamples; i++) xsum = xsum + elas(eb,theta + dtheta*(Math.random()-0.5));
         return nsamples*xsect/xsum;
     }
     
     public double elasq(double eb, double q2) {  
-    	double cx = 1-q2/(2*eb*(eb-q2/2/rmp));
+        double cx = 1-q2/(2*eb*(eb-q2/2/rmp));
 //    	double ep = eb-q2/2/rmp;
 //      float jac = pi*(1+q2/(2*ep*rmp))/(ep*eb);
-    	double epr = eb/(1+eb*(1-cx));
-    	double jac = epr*epr/pi;
-    	double th  = Math.toDegrees(Math.acos(cx));    
-    	return elas(eb,th)*jac;
+        double epr = eb/(1+eb*(1-cx));
+        double jac = epr*epr/pi;
+        double th  = Math.toDegrees(Math.acos(cx));    
+        return elas(eb,th)*jac;
     }
     
     public double elasradq(double eb, double q2, double t, double wcut) {
-    	double cx = 1-q2/(2*eb*(eb-q2/2/rmp));
+        double cx = 1-q2/(2*eb*(eb-q2/2/rmp));
 //    	double ep = eb-q2/2/rmp;
 //      float jac = pi*(1+q2/(2*ep*0.938))/(ep*eb);
-    	double epr=eb/(1+eb*(1-cx));
-    	double jac=epr*epr/pi;
-    	double th=Math.toDegrees(Math.acos(cx));    
-    	return radcor(eb,th,0,t,0.,0.,wcut,0)*jac;
+        double epr=eb/(1+eb*(1-cx));
+        double jac=epr*epr/pi;
+        double th=Math.toDegrees(Math.acos(cx));    
+        return radcor(eb,th,0,t,0.,0.,wcut,0)*jac;
     }
 
     
     public double asym(double e0, double q2, int param, int out) {
-    	double[] aout = new double[3];
-    	double nu 	= q2/2/rmp;
-    	double e_pr	= e0-nu;
-    	if (e_pr<=0.) return 0;
-    	double tau	= q2/4/rmp/rmp;
-    	double v_l	= 1/Math.pow(1+tau,2);
-    	double snth2	= q2/4/e0/e_pr;
-    	double csth2	= 1-snth2;
-    	if (csth2<=0 || snth2<=0) return 0;
-    	snth2	= Math.sqrt(snth2);
-    	csth2	= Math.sqrt(csth2);
-    	double tnth2	= snth2/csth2;
-//    	double theta_e	= Math.toDegrees(Math.asin(snth2));
-    	double x		= tnth2*tnth2;
-    	double y		= 1./(1.+tau);
-    	double v_t	= y/2 + x;
-    	double v_tpr	=  tnth2*Math.sqrt(x+y);
-    	double v_tlpr= -tnth2*y/Math.sqrt(2.);
+        double[] aout = new double[3];
+        double nu 	= q2/2/rmp;
+        double e_pr	= e0-nu;
+        if (e_pr<=0.) return 0;
+        double tau	= q2/4/rmp/rmp;
+        double v_l	= 1/Math.pow(1+tau,2);
+        double snth2	= q2/4/e0/e_pr;
+        double csth2	= 1-snth2;
+        if (csth2<=0 || snth2<=0) return 0;
+        snth2	= Math.sqrt(snth2);
+        csth2	= Math.sqrt(csth2);
+        double tnth2	= snth2/csth2;
+//      double theta_e	= Math.toDegrees(Math.asin(snth2));
+        double x		= tnth2*tnth2;
+        double y		= 1./(1.+tau);
+        double v_t	= y/2 + x;
+        double v_tpr	=  tnth2*Math.sqrt(x+y);
+        double v_tlpr= -tnth2*y/Math.sqrt(2.);
+       
+        getFF(q2,param);
     
-    	getFF(q2,param);
-    
-    	double ge2	= gep*gep;
-    	double gm2	= gmp*gmp;
-    	double den	= v_l*(1.+tau)*ge2/gm2 + 2.*tau*v_t;
-    	double qvec	= Math.sqrt((1.+tau)*q2);
-    	double  snth_gam	= 2*snth2*csth2*e_pr/qvec;
-    	double  csth_gam	= Math.sqrt(1. - snth_gam*snth_gam);
-//    	double theta_gam	= Math.toDegrees(Math.asin(snth_gam));
-    	aout[0]	=  2*tau*v_tpr/den;
-    	aout[1]	= -2*Math.sqrt(2.*tau*(1.+tau))*gep/gmp*v_tlpr/den;
-    	aout[2]	= aout[0]*csth_gam+aout[1]*snth_gam;    
-    	return aout[out];
+        double ge2	= gep*gep;
+        double gm2	= gmp*gmp;
+        double den	= v_l*(1.+tau)*ge2/gm2 + 2.*tau*v_t;
+        double qvec	= Math.sqrt((1.+tau)*q2);
+        double  snth_gam	= 2*snth2*csth2*e_pr/qvec;
+        double  csth_gam	= Math.sqrt(1. - snth_gam*snth_gam);
+//      double theta_gam	= Math.toDegrees(Math.asin(snth_gam));
+        aout[0]	=  2*tau*v_tpr/den;
+        aout[1]	= -2*Math.sqrt(2.*tau*(1.+tau))*gep/gmp*v_tlpr/den;
+        aout[2]	= aout[0]*csth_gam+aout[1]*snth_gam;    
+        return aout[out];
     }
     
 
@@ -121,96 +122,96 @@ public class MoTsai {
     	
     	// gep,gmn form factor parameterizations 1=dipole ff 2=Bosted ff 3=Brash ff 7=A1-Mainz
    	
-    	double corr1, corr2, q1 = Math.sqrt(q2);
+        double corr1, corr2, q1 = Math.sqrt(q2);
     
-    	switch (opt) {    
-    	case 1: // dipole:
-    		gep	= 1./Math.pow((1.+q2/0.71),2);
-    		gmp	= 2.7928*gep;
-    		break;
-    	case 2:// Bosted parameterization of form factors: Phys. Rev. C 51, 409
-    		corr1 = 1 + 0.62*q1 + 0.68*q2 + 2.8*q2*q1 + 0.83*q2*q2;
-    		corr2 = 1 + 0.35*q1 + 2.44*q2 + 0.5*q2*q1 + 1.04*q2*q2 + 0.34*q2*q2*q1;
-    		gep   = 1./corr1;
-    		gmp   = 2.7928/corr2;
+        switch (opt) {    
+        case 1: // dipole:
+            gep	= 1./Math.pow((1.+q2/0.71),2);
+            gmp	= 2.7928*gep;
+           break;
+        case 2:// Bosted parameterization of form factors: Phys. Rev. C 51, 409
+            corr1 = 1 + 0.62*q1 + 0.68*q2 + 2.8*q2*q1 + 0.83*q2*q2;
+            corr2 = 1 + 0.35*q1 + 2.44*q2 + 0.5*q2*q1 + 1.04*q2*q2 + 0.34*q2*q2*q1;
+            gep   = 1./corr1;
+            gmp   = 2.7928/corr2;
             break;
     	case 3: //Brash parameterization of Hall A GeP/GmP measurement: hep-ex\0111038 PRC 65, 051001
-    		corr1 = 1 + 0.116*q1 + 2.874*q2 + 0.241*q2*q1 + 1.006*q2*q2 + 0.345*q2*q2*q1;
-    		corr2 = 1 - 0.13*(q2-0.04);
-    		gep   = corr2/corr1;
-    		gmp   = 2.7928/corr1;
+            corr1 = 1 + 0.116*q1 + 2.874*q2 + 0.241*q2*q1 + 1.006*q2*q2 + 0.345*q2*q2*q1;
+            corr2 = 1 - 0.13*(q2-0.04);
+            gep   = corr2/corr1;
+            gmp   = 2.7928/corr1;
             break;
-    	case 4:     
-    		corr2 = 1 + 0.35*q1 + 2.44*q2 + 0.5*q2*q1 + 1.04*q2*q2 + 0.34*q2*q2*q1;
-    		gmp   = 2.7928/corr2;
-    		gep   = 0.0;
-    		break;
-    	case 5: 
-    		gep = 1.041/Math.pow(1+q2/0.765,2) - 0.041/Math.pow(1+q2/6.2,2);
-    		gmp = 1.002/Math.pow(1+q2/0.749,2) - 0.002/Math.pow(1+q2/6.0,2);
-    		gmp = 2.7928*gmp;
-    		break;
-    	case 6:
-    		gep = 1.041/Math.pow(1+q2/0.765,2) - 0.041/Math.pow(1+q2/6.2,2);
-    		gmp = 1.002/Math.pow(1+q2/0.749,2) - 0.002/Math.pow(1+q2/6.0,2);
-    		double gepb = Math.exp(-0.5*Math.pow((q1-0.07)/0.27,2)) + Math.exp(-0.5*Math.pow((q1+0.07)/0.27,2));
-    		gep = gep - 0.3*q2*gepb;    
-    		double gmpb = Math.exp(-0.5*Math.pow((q1-0.35)/0.21,2)) + Math.exp(-0.5*Math.pow((q1+0.35)/0.21,2));
-    		gmp = 2.7928*(gmp - 0.16*q2*gmpb);
-    		break;
+        case 4:     
+            corr2 = 1 + 0.35*q1 + 2.44*q2 + 0.5*q2*q1 + 1.04*q2*q2 + 0.34*q2*q2*q1;
+            gmp   = 2.7928/corr2;
+            gep   = 0.0;
+            break;
+        case 5: 
+            gep = 1.041/Math.pow(1+q2/0.765,2) - 0.041/Math.pow(1+q2/6.2,2);
+            gmp = 1.002/Math.pow(1+q2/0.749,2) - 0.002/Math.pow(1+q2/6.0,2);
+            gmp = 2.7928*gmp;
+            break;
+        case 6:
+            gep = 1.041/Math.pow(1+q2/0.765,2) - 0.041/Math.pow(1+q2/6.2,2);
+            gmp = 1.002/Math.pow(1+q2/0.749,2) - 0.002/Math.pow(1+q2/6.0,2);
+            double gepb = Math.exp(-0.5*Math.pow((q1-0.07)/0.27,2)) + Math.exp(-0.5*Math.pow((q1+0.07)/0.27,2));
+            gep = gep - 0.3*q2*gepb;    
+            double gmpb = Math.exp(-0.5*Math.pow((q1-0.35)/0.21,2)) + Math.exp(-0.5*Math.pow((q1+0.35)/0.21,2));
+            gmp = 2.7928*(gmp - 0.16*q2*gmpb);
+            break;
 //    	case 7: //Direct extraction of Ge and Gm from A1-Mainz data (Bernauer, arXiv:1307.6227v1)          
 //      	gep = divdif(spige,spix,1000,q2,2)
 //      	gmp = 2.7928*divdif(spigm,spix,1000,q2,2)
     	}
 
-    	return;
+        return;
     }
     
     public double spence(double x) {
-    	double spence;
-    	if (Math.abs(x)<0.1) {
-    		spence = x+x*x/4.;
-    	} else if (x>0.99 && x<1.01) {
-    		spence = pi*pi/6.;
-    	} else if (x>-1.01 && x<-0.99) {
-    		spence = -pi*pi/12.;
-    	} else if (x>0) {
-    		spence =  0.1025+sintp(x);
-    	} else {
-    		spence = -0.0975+sintn(x);
-    	}    
-    	return spence;
+        double spence;
+        if (Math.abs(x)<0.1) {
+            spence = x+x*x/4.;
+        } else if (x>0.99 && x<1.01) {
+            spence = pi*pi/6.;
+        } else if (x>-1.01 && x<-0.99) {
+            spence = -pi*pi/12.;
+        } else if (x>0) {
+            spence =  0.1025+sintp(x);
+        } else {
+            spence = -0.0975+sintn(x);
+        }    
+        return spence;
     }
     
     public double sintp(double x) {
-    	double   arg,sum = 0.;
-    	double xstep = (x-0.1)/100.;
-    	double     y = 0.1-xstep/2.;
-    	for (int i=1; i<101; i++) {
-    		y = y+xstep;
-    		arg = Math.abs(1.-y);
-    		sum = sum - Math.log(arg)/y;
-    	}
-    	return sum*xstep;
+        double   arg,sum = 0.;
+        double xstep = (x-0.1)/100.;
+        double     y = 0.1-xstep/2.;
+        for (int i=1; i<101; i++) {
+            y = y+xstep;
+            arg = Math.abs(1.-y);
+            sum = sum - Math.log(arg)/y;
+        }
+        return sum*xstep;
     }
     
     public double sintn(double x) {
-    	double   sum = 0.;
-    	double    xa = Math.abs(x);
-    	double ystep =(xa-0.1)/100.;
-    	double     y = 0.1-ystep/2.;
-    	for (int i=1; i<101; i++) {
-    		y 	= y+ystep;
-    		sum = sum - Math.log(1+y)/y;
-    	}
-    	return sum*ystep;
+        double   sum = 0.;
+        double    xa = Math.abs(x);
+        double ystep =(xa-0.1)/100.;
+        double     y = 0.1-ystep/2.;
+        for (int i=1; i<101; i++) {
+            y 	= y+ystep;
+            sum = sum - Math.log(1+y)/y;
+        }
+        return sum*ystep;
     }
     
     public double bfunc(double z) {
-    	double xi1 = Math.log(1440.) - 2.*Math.log(z)/3.;
-    	double xi2 = Math.log(183.)  -    Math.log(z)/3.;
-    	double xi  = xi1/xi2;
-    	return (4./3.)*(1.+(z+1.)/(z+xi)/xi2/9.);
+        double xi1 = Math.log(1440.) - 2.*Math.log(z)/3.;
+        double xi2 = Math.log(183.)  -    Math.log(z)/3.;
+        double xi  = xi1/xi2;
+        return (4./3.)*(1.+(z+1.)/(z+xi)/xi2/9.);
     }
     
     public double radcor(double es, double theta_d, int izn, double t1, double t2, double t3, double wcut, int rc) {
@@ -462,69 +463,72 @@ public class MoTsai {
     
     public void tsai_table1() {
     	
-    	Formatter fmt = new Formatter();
-    	DecimalFormat df = new DecimalFormat("#.####");
+        Formatter fmt = new Formatter();
+        DecimalFormat df = new DecimalFormat("#.####");
     	
-    	setFF(1);
+        setFF(1);
 
         double[] eb = {17.314, 15.999, 14.649, 13.329, 11.999, 10.723, 6.032, 2.201, 2.206};
-    	double[] angle = {35.1, 19.7, 18.8, 17.6, 16.082, 14, 17.186, 38.601, 15.999};
+        double[] angle = {35.1, 19.7, 18.8, 17.6, 16.082, 14, 17.186, 38.601, 15.999};
     	
-    	fmt.format("%10s %10s %8s %8s %10s %10s %10s\n","Ebeam","Angle","Eelec","-q2","Z0","Z1","Z2");
-    	for (int i=0; i<eb.length; i++) {
-    		radcor(eb[i],angle[i],0,0.0058,0.0058,0.0,0,1); //wcut=0.1117 for W=1.05 GeV, wcut=0 for Mo&Tsai Table 1   		
-    		fmt.format("%10s %10s %8s %8s %10s %10s %10s\n",
-    				   eb[i],angle[i],df.format(e_prime),df.format(q2),df.format(z0sum),df.format(z1sum),df.format(z2sum));
+        fmt.format("%10s %10s %8s %8s %10s %10s %10s\n","Ebeam","Angle","Eelec","-q2","Z0","Z1","Z2");
+        for (int i=0; i<eb.length; i++) {
+            radcor(eb[i],angle[i],0,0.0058,0.0058,0.0,0,1); //wcut=0.1117 for W=1.05 GeV, wcut=0 for Mo&Tsai Table 1   		
+            fmt.format("%10s %10s %8s %8s %10s %10s %10s\n",
+            eb[i],angle[i],df.format(e_prime),df.format(q2),df.format(z0sum),df.format(z1sum),df.format(z2sum));
     	}
-    	System.out.println(fmt);
+        System.out.println(fmt);
     }
     
-    public void rc(double ebeam, double thmin, double thmax, double bw) {
+    public void rc(double ebeam, double thmin, double thmax, double bw, double wc) {
     	
-    	Formatter fmt = new Formatter();
-    	DecimalFormat df = new DecimalFormat("#.###");
-    	DecimalFormat dfx = new DecimalFormat("####.###");
+        Formatter fmt = new Formatter();
+        DecimalFormat df = new DecimalFormat("#.###");
+        DecimalFormat dfx = new DecimalFormat("####.####");
     	
-    	String format = "%8s %5s %5s %5s %9s %9s %7s %7s %7s %7s\n";
+        String format = "%8s %5s %5s %5s %9s %9s %7s %7s %7s %7s\n";
     	
-    	setFF(2);
+        setFF(2);
 
-    	fmt.format(format,"Ebeam","Angle","Eelec","-q2","xsraw(nb)","xsrad(nb)","rc_int","rc_ext","rc","bcc");
+        fmt.format(format,"Ebeam","Angle","Eelec","-q2","xsraw(nb)","xsrad(nb)","rc_int","rc_ext","rc","bcc");
 
-    	for (double theta=thmin/bw; theta<thmax/bw; theta++) {
-    		double the=theta*bw;
-    		radcor(ebeam,the,0,0.0058,0.0058,0.0,0,1);
-    		fmt.format(format,
-    		ebeam,df.format(the),df.format(e_prime),
-    		            df.format(q2),
-    		            dfx.format(xsect_raw*1e3),
-    		            dfx.format(xsect_rad*1e3),
-    		            df.format(del_mo),
-    		            df.format(delta_t),
-    		            df.format(radcor),
-    		            df.format(getBCC(ebeam,the,bw,10000)));
-    	}
-    	System.out.println(fmt);
+        for (double theta=thmin/bw; theta<thmax/bw; theta++) {
+            double the=theta*bw;
+            radcor(ebeam,the,0,0.0058,0.0058,0.0,wc-rmp,1);
+            fmt.format(format,
+            ebeam,df.format(the),df.format(e_prime),
+                  df.format(q2),
+                 dfx.format(xsect_raw*1e3),
+                 dfx.format(xsect_rad*1e3),
+                  df.format(del_mo),
+                  df.format(delta_t),
+                  df.format(radcor),
+                  df.format(getBCC(ebeam,the,bw,10000)));
+        }
+        System.out.println(fmt);
     }
     
     public void test3() {
-    	double[] x = {0.1,0.2,0.3,0.8,1.0,2.0,3.0,10,20,30,40};
-    	for (int i=0; i<x.length; i++) System.out.println(x[i]+" "+spence(x[i])+" "+spence(-x[i]));
+        double[] x = {0.1,0.2,0.3,0.8,1.0,2.0,3.0,10,20,30,40};
+        for (int i=0; i<x.length; i++) System.out.println(x[i]+" "+spence(x[i])+" "+spence(-x[i]));
     }
     
     public static void main(String[] args) {  
-    	
-     	
-     	if (args.length!=0) {
-     	   	MoTsai elib = new MoTsai();  
-    		if(args[0].equals("demo")) elib.tsai_table1();
-    		if(args[0].equals("table")) {
-    			elib.rc(Double.parseDouble(args[1]), 
-    					Double.parseDouble(args[2]),
-    					Double.parseDouble(args[3]),
-    					Double.parseDouble(args[4]));
-    		}
-    	}
+    	     	
+        if (args.length!=0) {
+            MoTsai elib = new MoTsai();  
+            if(args[0].equals("demo")) elib.tsai_table1();
+            if(args[0].equals("table")&&args.length==1) {
+            	elib.rc(7.645, 5, 30, 0.1, 1.05); return;
+            }
+            if(args[0].equals("table")) {
+                elib.rc(Double.parseDouble(args[1]), 
+                        Double.parseDouble(args[2]),
+                        Double.parseDouble(args[3]),
+                        Double.parseDouble(args[4]),
+                        Double.parseDouble(args[5]));
+            }
+        }
     }
 
 }
